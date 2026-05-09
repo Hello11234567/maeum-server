159b9407c7f899857c29bb15d43570c3314e802a
Hello11234567 <lia4031@naver.com>
2026-05-09 22:55:43 +0900

    chore: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/maeum/maeum/MaeumApplication.java
@@ -0,0 +1,13 @@
+package com.maeum.maeum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaeumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaeumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maeum/maeum/MaeumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maeum.maeum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaeumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
